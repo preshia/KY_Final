24ad038a01f2c7aa969bcfe6fc2970f650d33d34
preshia <preshia@naver.com>
2017-05-31 10:08:45 +0900

    첫 올림.

---
+++ Final_Project/Assets/CMapGenerator.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMapGenerator : MonoBehaviour {
+
+    public int mWidth;
+    public int mHeight;
+
+    public string mSeed;
+    public bool mUseRandomSeed;
+
+    [Range(0,100)]
+    public int mRandomFillPercent;
+    int[,] mMap;
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GenerateMap();
+        }
+    }
+
+    void GenerateMap()
+    {
+        mMap = new int[mWidth, mHeight];
+        RandomFillMap();
+
+        for(int ti = 0; ti<5; ti++)
+        {
+            SmoothMap();
+        }
+
+        CMeshGenerator tMeshGen = this.gameObject.GetComponent<CMeshGenerator>();
+        tMeshGen.GenerateMesh(mMap, 1);
+    }
+
+    void RandomFillMap()
+    {
+        if(true == mUseRandomSeed)
+        {
+            mSeed = Time.time.ToString();
+        }
+
+        System.Random pseudoRandom = new System.Random(mSeed.GetHashCode());
+
+        
+
+        for (int tx = 0; tx<mWidth; tx++)
+        {
+            for(int ty = 0; ty<mHeight; ty++)
+            {
+                if(0 == tx || mWidth - 1 == tx || 0 == ty || mHeight-1 == ty)
+                {
+                    mMap[tx, ty] = 1;
+                }
+                else
+                {
+                    mMap[tx, ty] = (pseudoRandom.Next(0, 100) < mRandomFillPercent) ? 1 : 0;
+                }
+            }
+        }
+    }
+
+    void SmoothMap()
+    {
+        for (int tx = 0; tx < mWidth; tx++)
+        {
+            for (int ty = 0; ty < mHeight; ty++)
+            {
+                int tNeighbourWalltiles = GetSurroundingWallCount(tx, ty);
+
+                if(tNeighbourWalltiles>4)
+                {
+                    mMap[tx, ty] = 1;
+                }
+                else if(tNeighbourWalltiles < 4)
+                {
+                    mMap[tx, ty] = 0;
+                }
+            }
+        }
+    }
+
+    int GetSurroundingWallCount(int tGridX, int tGridY)
+    {
+        int tWallCount = 0;
+        for(int tNeighbourX = tGridX-1; tNeighbourX<=tGridX + 1; tNeighbourX++)
+        {
+            for (int tNeighbourY = tGridY - 1; tNeighbourY <= tGridY + 1; tNeighbourY++)
+            {
+                if(tNeighbourX>=0 && tNeighbourX<mWidth && tNeighbourY >=0 && tNeighbourY<mHeight)
+                {
+                    if (tNeighbourX != tGridX || tNeighbourY != tGridY)
+                    {
+                        tWallCount += mMap[tNeighbourX, tNeighbourY];
+                    }
+                }
+                else
+                {
+                    ++tWallCount;
+                }
+            }
+        }
+        return tWallCount;
+    }
+
+    private void OnDrawGizmos()
+    {
+        /*
+        if(null != mMap)
+        {
+            for (int tx = 0; tx < mWidth; tx++)
+            {
+                for (int ty = 0; ty < mHeight; ty++)
+                {
+                    Gizmos.color = (mMap[tx, ty] == 1) ? Color.black : Color.white;
+                    Vector3 pos = new Vector3(-mWidth / 2 + tx + 0.5f, 0, -mHeight / 2 + ty + 0.5f);
+                    Gizmos.DrawCube(pos, Vector3.one);
+                }
+            }
+        }
+        */
+    }
+}
---
+++ Final_Project/Assets/CMeshGenerator.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMeshGenerator : MonoBehaviour {
+
+
+    public CSquareGrid mSquareGrid;
+
+    public void GenerateMesh(int[,] tMap, float tSquareSize)
+    {
+        mSquareGrid = new CSquareGrid(tMap, tSquareSize);
+    }
+
+    public void OnDrawGizmos()
+    {
+        if(null != mSquareGrid)
+        {
+            for(int tx = 0; tx < mSquareGrid.mSquares.GetLength(0); ++tx)
+            {
+                for(int ty = 0; ty<mSquareGrid.mSquares.GetLength(1); ++ty)
+                {
+                    Gizmos.color = (mSquareGrid.mSquares[tx, ty].mTopLeft.mIsActive) ? Color.black : Color.white;
+                    Gizmos.DrawCube(mSquareGrid.mSquares[tx, ty].mTopLeft.mPosition, Vector3.one * 0.4f);
+
+                    Gizmos.color = (mSquareGrid.mSquares[tx, ty].mTopRight.mIsActive) ? Color.black : Color.white;
+                    Gizmos.DrawCube(mSquareGrid.mSquares[tx, ty].mTopRight.mPosition, Vector3.one * 0.4f);
+
+                    Gizmos.color = (mSquareGrid.mSquares[tx, ty].mBottomLeft.mIsActive) ? Color.black : Color.white;
+                    Gizmos.DrawCube(mSquareGrid.mSquares[tx, ty].mBottomLeft.mPosition, Vector3.one * 0.4f);
+
+                    Gizmos.color = (mSquareGrid.mSquares[tx, ty].mBottomRight.mIsActive) ? Color.black : Color.white;
+                    Gizmos.DrawCube(mSquareGrid.mSquares[tx, ty].mBottomRight.mPosition, Vector3.one * 0.4f);
+
+
+                    Gizmos.color = Color.grey;
+                    Gizmos.DrawCube(mSquareGrid.mSquares[tx, ty].mCenterTop.mPosition, Vector3.one * 0.15f);
+                    Gizmos.DrawCube(mSquareGrid.mSquares[tx, ty].mCenterRight.mPosition, Vector3.one * 0.15f);
+                    Gizmos.DrawCube(mSquareGrid.mSquares[tx, ty].mCenterBottom.mPosition, Vector3.one * 0.15f);
+                    Gizmos.DrawCube(mSquareGrid.mSquares[tx, ty].mCenterLeft.mPosition, Vector3.one * 0.15f);
+                }
+            }
+        }
+    }
+
+
+
+    /// <summary>
+    /// ////////////////////////////////////////////////////////////////////////////////
+    /// </summary>
+
+
+
+    public class CSquareGrid
+    {
+        public CSquare[,] mSquares;
+        public CSquareGrid(int[,] tMap, float tSquareSize)
+        {
+            int tNodeCountX = tMap.GetLength(0);
+            int tNodeCountY = tMap.GetLength(1);
+
+            float tMapWidth = tNodeCountX * tSquareSize;
+            float tMapHeight = tNodeCountY * tSquareSize;
+
+            CControlNode[,] tControlNodes = new CControlNode[tNodeCountX, tNodeCountY];
+
+            Vector3 tPos;
+            for(int tx = 0; tx<tNodeCountX; tx++)
+            {
+                for(int ty = 0; ty<tNodeCountY; ty++)
+                {
+                    tPos = new Vector3(-tMapWidth / 2 + tx * tSquareSize + tSquareSize / 2, 0, -tMapHeight / 2 + ty * tSquareSize + tSquareSize / 2);
+                    tControlNodes[tx, ty] = new CControlNode(tPos, tMap[tx, ty] == 1, tSquareSize);
+                }
+            }
+
+            mSquares = new CSquare[tNodeCountX - 1, tNodeCountY - 1];
+            for (int tx = 0; tx < tNodeCountX-1; tx++)
+            {
+                for (int ty = 0; ty < tNodeCountY-1; ty++)
+                {
+                    mSquares[tx, ty] = new CSquare(tControlNodes[tx, ty + 1], tControlNodes[tx + 1, ty + 1], tControlNodes[tx + 1, ty], tControlNodes[tx, ty]);
+                }
+            }
+        }
+    }
+
+    public class CSquare
+    {
+        public CControlNode mTopLeft, mTopRight, mBottomRight, mBottomLeft;
+        public CNode mCenterTop, mCenterRight, mCenterBottom, mCenterLeft;
+
+        public CSquare(CControlNode tTopLeft, CControlNode tTopRight, CControlNode tBottomRight, CControlNode tBottomLeft)
+        {
+            mTopLeft = tTopLeft;
+            mTopRight = tTopRight;
+            mBottomRight = tBottomRight;
+            mBottomLeft = tBottomLeft;
+
+            mCenterTop = mTopLeft.mRight;
+            mCenterRight = mBottomRight.mAbove;
+            mCenterBottom = mBottomLeft.mRight;
+            mCenterLeft = mBottomLeft.mAbove;
+        }
+    }
+    
+	public class CNode
+    {
+        public Vector3 mPosition;
+        public int mVertexIndex = -1;
+
+        public CNode(Vector3 tPos)
+        {
+            mPosition = tPos;
+        }
+    }
+
+    public class CControlNode : CNode
+    {
+        public bool mIsActive;
+        public CNode mAbove, mRight;
+
+        public CControlNode(Vector3 tPos, bool tActive, float tSquareSize) : base(tPos)
+        {
+            mIsActive = tActive;
+            mAbove = new CNode(mPosition + Vector3.forward * tSquareSize / 2.0f);
+            mRight = new CNode(mPosition + Vector3.right * tSquareSize / 2.0f);
+        }
+
+    }
+}
